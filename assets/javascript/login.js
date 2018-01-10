c446fe06c85d7a308372865af43b8cbd9fa556dd
Christian Escobar <crs.escobar@gmail.com>
2018-01-10 11:10:37 -0800

    various changes

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -31,10 +31,10 @@ $("#user-login-btn").on("click", function(event){
   loginUser(userEmail, password);
 });
 
-$("#google-login-btn").on("click", function(){
+$("#fb-login-btn").on("click", function(){
   console.log("click");
 
-  loginWithGoogle();
+  loginWithFacebook();
 });
 
 function createUser(userEmail, password) {
@@ -65,28 +65,27 @@ function loginUser(userEmail, password) {
   });
 }
 
-function loginWithGoogle() {
-  var provider = new firebase.auth.GoogleAuthProvider();
+function loginWithFacebook() {
+  var provider = new firebase.auth.FacebookAuthProvider();
 
   authentication.signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
-    
+
     console.log(user);
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-
-    console.log(errorCode + " " + errorMessage);
-
     // The email of the user's account used.
     var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
     // ...
+
+    console.log(errorCode + " " + errorMessage);
   });
 }
 
